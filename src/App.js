a7b7a4a3616c243088e283ead03eda62f0266dab
Gonzalo Roa <gonzalo@multiplica.com>
2022-08-02 21:46:27 -0500

    chore: create react app initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [response, setResponse] = useState('');
+  useEffect(() => {
+    fetch('http://localhost:4000/health').then(res => res.json()).then(data => {
+      setResponse(data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Estado del servicio:: { response }</p>
         <a
           className="App-link"
           href="https://reactjs.org"
